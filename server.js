8da44684ea1307d2d44a2df98470517c15fe6714
Honneure <clement.dutartre@gmail.com>
2016-11-23 16:54:54 +0000

    fixing

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
-var app = express()
+var app = express();
 var arg = process.argv[2];
 var moment = require("moment");
 var time = require("./timestamp.js");
 
 app.get('/', function (req, res) {
-  res.send(time(arg))
-})
+  res.send(time(arg));
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 5000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 
 
